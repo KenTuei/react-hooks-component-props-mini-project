18188863769d4d253c7c85940087edb60d43a809
ken <kentuei05@gmail.com>
2025-04-16 18:55:56 +0300

    Transforming lists of data into lists of components

---
+++ src/components/About.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function About({ image = "https://via.placeholder.com/215", about }) { // Updated prop name to "about"
+    return (
+        <aside>
+            <img src={image} alt="blog logo" />
+            <p>{about}</p> {/* Updated prop name */}
+        </aside>
+    )
+}
+
+export default About;
